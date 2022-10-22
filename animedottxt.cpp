615587f23658550595290f0ad86763f0822da972
Zese07 <42308638+Zese07@users.noreply.github.com>
2022-10-22 15:57:42 +0800

    Added 10 new characters.

--- animedottxt.cpp
+++ animedottxt.cpp
@@ -37,7 +37,7 @@ class gameTxt{
 			    	break;
 			    default:
 			    	cout << "                           Exiting..." << endl;
-                    cout << space << endl;
+                   	 	cout << space << endl;
 
 	   		 	break;
 
@@ -46,14 +46,13 @@ class gameTxt{
 		void txtStart(){
 			srand(time(0));
 
-            int column = txtColumn();
-		    int row = txtRow();
-		    int rowadd = txtRowAdd();
+            		int column = txtColumn();
+		    	int row = txtRow();
+		    	int rowadd = txtRowAdd();
 
 			switch(column){
-                case 0: txt0(row, rowadd); break;
-                case 1: txt1(row, rowadd); break;
-                default: cout << "Error."; break;
+                		case 0: txt0(row, rowadd); break;
+                		default: cout << "Error."; break;
 
 			}
 		}
@@ -69,7 +68,7 @@ class gameTxt{
 		}
 		int txtRowAdd(){
 			int rowadd = rand()%5;
-			rowadd = rowadd * 0;
+			rowadd = rowadd * 2;
 			return rowadd;
 
 		}
@@ -89,10 +88,10 @@ class gameTxt{
 		}
 		void txtWrong(){
 			cout << "Wrong" << endl;
-	    	cout << space << endl;
-	    	cout << "                           Final Score: " << score << endl;
-	    	score = 0;
-	    	txtMenu();
+	    		cout << space << endl;
+	    		cout << "                           Final Score: " << score << endl;
+	    		score = 0;
+	    		txtMenu();
 
 		}
 		void txt0(int row, int rowadd){
@@ -105,9 +104,14 @@ class gameTxt{
 				case 1: case 2: case 3: case 4: case 6: clue = 'L'; break;
 				case 5: clue = 'Z'; break;
 				case 7: clue = 'R'; break;
-				case 8: clue = 'K'; break;
-				case 9: clue = 'E'; break;
+				case 8: case 13: case 18: clue = 'K'; break;
+				case 9: case 14: clue = 'E'; break;
 				case 10: clue = 'N'; break;
+				case 11: case 12: case 19: clue = 'G'; break;
+				case 15: clue = 'I'; break;
+				case 16: clue = 'M'; break;
+				case 17: clue = 'H'; break;
+				case 20: clue = 'S'; break;
 				default:
                     cout << "Error: " << row << endl;
                     txtWrong();
@@ -132,17 +136,24 @@ class gameTxt{
 			if(((name == "Lelouch") || (name == "Zero") || (name == "Lulu")) && row == 1){ txtCorrect();
 			}else if((name == "Levi") && row == 2){ txtCorrect();
 			}else if(((name == "Luffy") || (name == "Mugiwara") || (name == "Lucy")) && row == 3){ txtCorrect();
-	    	}else if(((name == "L") || (name == "Hideki") || (name == "Ryuzaki") || (name == "Coil") || (name == "Deneuve")) && row == 4){ txtCorrect();
-	    	}else if(((name == "Zoro") || (name == "Marimo") || (name == "Zorojuro")) && row == 5){ txtCorrect();
-	    	}else if(((name == "Light") || (name == "Kira")) && row == 6){ txtCorrect();
-	    	}else if(((name == "Rintarou") || (name == "Kyouma") || (name == "Okarin")) && row == 7){ txtCorrect();
-	    	}else if((name == "Killua") && row == 8){ txtCorrect();
-	    	}else if(((name == "Edward") || (name == "Ed")) && row == 9){ txtCorrect();
-	    	}else if((name == "Naruto") && row == 10){ txtCorrect();
-	    	}else{ txtWrong(); }
-
-		}
-		void txt1(int row, int rowadd){
+	    		}else if(((name == "L") || (name == "Hideki") || (name == "Ryuzaki") || (name == "Coil") || (name == "Deneuve")) && row == 4){ txtCorrect();
+	    		}else if(((name == "Zoro") || (name == "Marimo") || (name == "Zorojuro")) && row == 5){ txtCorrect();
+	    		}else if(((name == "Light") || (name == "Kira")) && row == 6){ txtCorrect();
+	    		}else if(((name == "Rintarou") || (name == "Kyouma") || (name == "Okarin")) && row == 7){ txtCorrect();
+	    		}else if((name == "Killua") && row == 8){ txtCorrect();
+	    		}else if(((name == "Edward") || (name == "Ed")) && row == 9){ txtCorrect();
+	    		}else if((name == "Naruto") && row == 10){ txtCorrect();
+	    		}else if((name == "Guts") && row == 11){ txtCorrect();
+	    		}else if(((name == "Gintoki") || (name == "Gin-san") || (name == "Gin-chan")) && row == 12){ txtCorrect();
+	    		}else if(((name == "Kurisu") || (name == "Christina")) && row == 13){ txtCorrect();
+	    		}else if((name == "Eren") && row == 14){ txtCorrect();
+	    		}else if((name == "Itachi") && row == 15){ txtCorrect();
+	    		}else if((name == "Mikasa") && row == 16){ txtCorrect();
+	    		}else if(((name == "Hachiman") || (name == "Hikki") || (name == "Hikitani") || (name == "Hikio") || (name == "Hikigaeru") || (name == "Hikigerma") || (name == "Fishigaya")) && row == 17){ txtCorrect();
+	    		}else if(((name == "Ken") || (name == "Centipede") || (name == "Dragon") || (name == "Eyepatch")) && row == 18){ txtCorrect();
+	    		}else if((name == "Satoru") && row == 19){ txtCorrect();
+	    		}else if((name == "Saitama") && row == 20){ txtCorrect();
+	    		}else{ txtWrong(); }
 
 		}
 };
